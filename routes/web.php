b1bf8d6ea1ce59d030530391efd6a3d7d7845b09
hilal1709 <hilal.andi123@gmail.com>
2025-11-08 22:49:07 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('welcome');
+})->name('home');
+
+Route::get('/damar-kurung', function () {
+    return Inertia::render('damar-kurung');
+})->name('damar-kurung');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+});
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
