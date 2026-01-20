459deffc339dc165cd9b50773f804f81e4a451d0
ainul yaqin <inulyaqin10@gmail.com>
2026-01-20 08:23:29 +0700

    route register

--- routes/auth.php
+++ routes/auth.php
@@ -21,10 +21,10 @@ Route::middleware('guest')->group(function () {
         return Inertia::render('auth/register');
     })->name('register');
 
-    Route::middleware('auth','role:admin' )->group(function() {
+    
         Route::middleware('guest')->post('/register', [RegisteredUserController::class, 'store'])
         ->name('register.create');
-    });
+    
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
